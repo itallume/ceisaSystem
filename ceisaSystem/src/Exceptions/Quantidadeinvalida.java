47e8dea940d7f9057d8a291f499640f1f3f7735b
AdielSM <129189553+AdielSM@users.noreply.github.com>
2024-04-05 14:06:09 -0300

    Exceptions

---
+++ ceisaSystem/src/Exceptions/Quantidadeinvalida.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class Quantidadeinvalida extends Exception{
+    public Quantidadeinvalida(String msg) {
+        super(msg);
+    }
+}
---
+++ ceisaSystem/src/Exceptions/ValorUnitarioInvalido.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class ValorUnitarioInvalido extends Exception{
+    public ValorUnitarioInvalido(String msg) {
+        super(msg);
+    }
+}
